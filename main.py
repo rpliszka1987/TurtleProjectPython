813cbe231bddab9cfbe138cd393ac2c0a124cdcb
Robert Pliszka <robplisz@gmail.com>
2026-09-16 18:11:42 -0400

    Created a turtle that moves and has a different color

---
+++ main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("blue1")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
